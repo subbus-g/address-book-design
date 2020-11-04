ac4dc94679c01e60339e161b74584cb6ca0fb78f
subbus <subbareddyg83@gmail.com>
2020-11-04 13:12:33 +0530

    added filling  the contact detailsl

--- AddressBookLauncher.java
+++ AddressBookLauncher.java
@@ -1,13 +1,18 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 class AddressBook 
 {
     // class variable
     static final Scanner scanner = new Scanner(System.in);
+    static Set<String> emptyContacts = new HashSet<>();
 
     // instance method
-    void createNewContact() throws Exception {
+    void createNewContact() throws Exception 
+    {
         System.out.print("Enter name of the contact:");
         String contactName = scanner.nextLine();
         File file = new File(contactName);
@@ -20,17 +25,54 @@ class AddressBook
             if (file.createNewFile()) 
             {
                 System.out.println("new contact " + file.getName() + " is created successfully");
+                emptyContacts.add(file.getName());
             } 
-            else 
-            {
+            else
+            { 
+            
                 System.out.println("file creation failed!");
             }
         }
 
     }
+
+    void fillContactDetails() throws Exception 
+    {
+        System.out.print("enter empty contact name which is going to be filed:");
+        String contactName = scanner.nextLine();
+        if (emptyContacts.contains(contactName))
+        {
+
+            FileWriter fw = new FileWriter(contactName);
+            String details = "";
+            System.out.print("enter first name:");
+            details += scanner.nextLine() + "\n";
+            System.out.print("enter last name:");
+            details += scanner.nextLine() + "\n";
+            System.out.print("enter address:");
+            details += scanner.nextLine() + "\n";
+            System.out.print("enter city:");
+            details += scanner.nextLine() + "\n";
+            System.out.print("enter state:");
+            details += scanner.nextLine() + "\n";
+            System.out.print("enter zip:");
+            details += scanner.nextLine() + "\n";
+            System.out.print("enter phone number:");
+            details += scanner.nextLine() + "\n";
+            fw.write(details);
+            fw.close();
+        }
+        else 
+        {
+            System.out.println(contactName + " is not empty contact or it is not created");
+            System.out.println("use other option 1 to create new contact");
+        }
+
+    }
 }
 
-public class AddressBookLauncher {
+public class AddressBookLauncher 
+{
     // class variable
     static final Scanner scanner = new Scanner(System.in);
 
@@ -41,7 +83,8 @@ public class AddressBookLauncher {
         {
             System.out.println("-----------------------------");
             System.out.println("1.create a new contact");
-            System.out.println("2.exit");
+            System.out.println("2.fill contact details");
+            System.out.println("3.exit");
             System.out.print("enter option:");
             int option = scanner.nextInt();
             switch (option) 
@@ -50,6 +93,9 @@ public class AddressBookLauncher {
                     addressBook.createNewContact();
                     break;
                 case 2:
+                    addressBook.fillContactDetails();
+                    break;
+                case 3:
                     System.out.println("exiting....");
                     System.exit(0);
                 default:
